f644632d8d619fecdace55d4304c2c1a69264db4
Bryce Patterson <44825546+BryceP-44@users.noreply.github.com>
2022-04-04 20:22:15 -0400

    Add files via upload

---
+++ desolver.py
@@ -0,0 +1,33 @@
+from math import *
+import matplotlib.pyplot as plt
+
+# y'' + 11y' = -24y
+#y(0)=0
+#yp(0)=-7
+
+t=0 #initial time
+y=0 #initial y
+yp=-7 #initial y'
+tf=10 #final time
+
+graph=[]
+rang=10000 #total number of points
+dt=(tf-t)/rang
+tl=[]
+t+=10**-300
+
+for i in range(rang):
+    ypp=1/(y*sin(t)) #solve for highest order (do on paper first)
+    yp+=ypp*dt #adjust all y values less than highest order
+    y+=yp*dt #adjust
+    tl.append(t) 
+    t=t+dt #increment time
+    graph.append(y)
+    
+
+print("estimate at t =",round(t,3),"is:",graph[len(graph)-1])
+
+
+
+plt.plot(tl,graph)
+plt.show()
